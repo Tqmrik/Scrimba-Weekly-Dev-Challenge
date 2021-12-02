a6809232dde20e908a84d647d5ecf76d4f01a61d
Tqmrik <steamxxlss@mail.ru>
2021-12-02 20:35:21 +0300

    Magic8-Ball

--- Magic8-Ball/index.js
+++ Magic8-Ball/index.js
@@ -1,4 +1,4 @@
-import randomcolor from "randomcolor"
+// import randomcolor from "randomcolor"
 
 const advice = [
     "If you’re good at something, never do it for free.",
@@ -8,6 +8,37 @@ const advice = [
     "Life does not stop and start at your convenience.",
     "You know what the trouble about real life is? There's no danger music."
 ]
+
+
+let attempts = 10
+if(localStorage.getItem("attempts"))
+{
+    attempts = localStorage.getItem("attempts")
+}
+
+
+const adviceButton = document.getElementById("adviceBtn")
+const textField = document.getElementById("adviceDisplay")
+const inptArea = document.getElementById("inptArea")
+
+inptArea.value = ""
+
+adviceButton.addEventListener("click",function()
+{
+    if(attempts > 0)
+    {
+        let rng = Math.floor(Math.random() * advice.length)
+        textField.textContent = advice[rng];
+        attempts--;
+        localStorage.setItem("attempts", attempts)
+    }
+    else
+    {
+        textField.textContent = "You are out of questions for today";
+    }
+    
+})
+
 
 // generate random light colors: 
     // adviceEye.style.backgroundColor = randomcolor({luminosity: 'light'})
