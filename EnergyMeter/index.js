d4ae36f03fb4b02fc5af0634a55d9c1d20ee88b6
Tqmrik <steamxxlss@mail.ru>
2021-12-04 17:06:58 +0300

    EnergyMeter

--- EnergyMeter/index.js
+++ EnergyMeter/index.js
@@ -1,1 +1,13 @@
 // woo JS 🥳
+
+const meter = document.getElementById("energy")
+const inpt = document.getElementById("input")
+
+meter.value = inpt.value
+
+inpt.addEventListener("change", function()
+{
+    meter.value = Math.sqrt(inpt.value * 10)
+    //change to any other math function if needed
+})
+
